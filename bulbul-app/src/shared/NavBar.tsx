2d74bdab77da1457d31d033527645761c994af48
Marius Šomka <marius.somka@mif.stud.vu.lt>
2023-07-10 21:11:08 +0300

    Initial project structure

---
+++ bulbul-app/src/shared/NavBar.tsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom";
+
+export const NavBar = () => {
+  return (
+    <div className="navbar bg-base-100 w-screen drop-shadow absolute inset-x-0 top-0">
+      <div className="flex-1">
+        <a className="btn btn-ghost normal-case text-xl">
+          <Link to={"/"}>
+            <span className="font-bold text-gray-800">{"Bulbul"}</span>
+          </Link>
+        </a>
+      </div>
+      <div className="flex-none">
+        <button className="btn btn-square btn-ghost">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            className="inline-block w-5 h-5 stroke-current"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+            ></path>
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+};
